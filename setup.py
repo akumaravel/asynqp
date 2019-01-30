0c4e67b36940f1e1168f94854f0d9157541c7f74
Anand Kumaravel <akumaravel@lifesize.com>
2019-01-30 14:33:38 -0600

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='asynqp',
-    version='0.8',
+    version='1.0',
     author="Benjamin Hodgson",
     author_email="benjamin.hodgson@huddle.net",
     url="https://github.com/benjamin-hodgson/asynqp",
